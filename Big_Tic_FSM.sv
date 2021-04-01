a8beb5419de1f29c4fd938c8a409d0d3cb823915
CMUBOB97 <73044785+CMUBOB97@users.noreply.github.com>
2021-04-01 18:53:11 -0400

    Task 5

---
+++ Big_Tic_FSM.sv
@@ -0,0 +1,10 @@
+`default_nettype none
+
+module myBigFSM
+  (output logic [3:0] cMove,
+   output logic win,
+   output logic [15:0] hMove_all, cMove_all,
+   input logic [3:0] hMove,
+   input logic enter, clock, reset);
+
+endmodule: myBigFSM
